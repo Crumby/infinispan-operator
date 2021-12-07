42e03db066019b40918b00944700bd7e123fa31c
Pavel Drobek <pdrobek@redhat.com>
2021-12-07 17:00:34 +0100

    [E2E] Do not creat OperatorGroup if Operator is installed globally

--- test/e2e/upgrade/upgrade_test.go
+++ test/e2e/upgrade/upgrade_test.go
@@ -74,7 +74,10 @@ func TestUpgrade(t *testing.T) {
 	}
 
 	defer cleanup(labels)
-	testKube.CreateOperatorGroup(subName, tutils.Namespace, tutils.Namespace)
+	// Create OperatorGroup only if Subscription is created in non-global namespace
+	if subNamespace != "openshift-operators" && subNamespace != "operators" {
+		testKube.CreateOperatorGroup(subName, subNamespace, subNamespace)
+	}
 	testKube.CreateSubscription(sub)
 
 	// Approve the initial startingCSV InstallPlan
@@ -88,7 +91,7 @@ func TestUpgrade(t *testing.T) {
 	})
 
 	// Create the Infinispan CR
-	replicas := 2
+	replicas := 3
 	spec := tutils.DefaultSpec(testKube)
 	spec.Name = name
 	spec.Labels = labels
@@ -200,9 +203,9 @@ func checkServicePorts(t *testing.T, name string) {
 	assert.Equal(t, 1, len(userPort))
 	assert.Equal(t, constants.InfinispanUserPort, int(userPort[0].Port))
 
-	adminPort := testKube.GetAdminServicePorts(tutils.Namespace, name)
-	assert.Equal(t, 1, len(adminPort))
-	assert.Equal(t, constants.InfinispanAdminPort, int(adminPort[0].Port))
+	// adminPort := testKube.GetAdminServicePorts(tutils.Namespace, name)
+	// assert.Equal(t, 1, len(adminPort))
+	// assert.Equal(t, constants.InfinispanAdminPort, int(adminPort[0].Port))
 }
 
 func checkBatch(name string) {
@@ -259,7 +262,7 @@ func populateCache(cacheName, host string, numEntries int, infinispan *ispnv1.In
 
 	headers := map[string]string{"Content-Type": "application/json"}
 	url := fmt.Sprintf("%s/rest/v2/caches/%s", host, cacheName)
-	config := `{"distributed-cache":{"mode":"SYNC", "persistence":{"file-store":{}}}}`
+	config := `{"distributed-cache":{"mode":"SYNC", "encoding": {"media-type": "application/json"}, "persistence":{"file-store":{}}, "statistics":true}}`
 	post(url, config, http.StatusOK, headers)
 
 	for i := 0; i < numEntries; i++ {
